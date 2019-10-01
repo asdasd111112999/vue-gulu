617d3e9d7b2898ce17506b354b64f38c3da3bb99
liuwenjun <570671435@qq.com>
2019-10-01 20:50:07 +0800

    添加click事件

--- src/app.js
+++ src/app.js
@@ -7,5 +7,10 @@ Vue.component('g-button',Buttom)
 Vue.component('g-icon',Icon)
 
 new Vue({
-    el:'#app'
+    el:'#app',
+    data:{
+        loading1:false,
+        loading2:false,
+        loading3:false
+    }
 })
--- src/icon.vue
+++ src/icon.vue
@@ -6,7 +6,7 @@
 
 <script>
     export default {
-        props:['name']
+        props: ['name']
     }
 </script>
 
