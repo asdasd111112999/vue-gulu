52c318b6b3daa65b7b3015cb208c4c8ca6966487
liuwenjun <570671435@qq.com>
2019-10-01 16:54:02 +0800

    添加了button的icon和position，并对position进行了验证

--- src/button.vue
+++ src/button.vue
@@ -1,15 +1,47 @@
 <template>
-    <button class="g-button">按钮</button>
+
+    <button class="g-button" :class="{[`icon-${position}`]:true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href=`#icon-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+
+    </button>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props:{
+            icon:{},
+            position:{
+                type:String,
+                default:'left',
+                validator: function (value) {
+                    return value === 'left' || value === 'right';
+                }
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
-    .g-button{font-size:var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--button-radius);border:1px solid var(--border-color);background: var(--button-bg);
-        &:hover{border-color: var(--border-color-hover);}
-        &:active{background-color:var(--button-active-bg);}
-        &:focus{outline: none;}
+    .g-button {font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--button-radius);border: 1px solid var(--border-color);background: var(--button-bg);display: inline-flex;justify-content: center;align-items: center;vertical-align: top;
+        &:hover {border-color: var(--border-color-hover);}
+        &:active {background-color: var(--button-active-bg);}
+        &:focus {outline: none;}
+        >.icon{order: 1; margin:  0 0.3em 0 0;}
+        >.content{order:2;}
+
+        &.icon-right{
+            >.icon{order:2;margin:  0 0 0 0.3em;}
+            >.content{order: 1;}
+        }
+    }
+
+    .icon {
+        width: 1em;
+        height: 1em;
     }
 </style>
