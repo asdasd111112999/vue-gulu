e71ed5a508f3bd091b84042676e934b90163fd47
liuwenjun <570671435@qq.com>
2019-10-02 15:05:05 +0800

    添加了按钮的单元测试

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
-import Buttom from './button';
+import Button from './button';
 import Icon from './icon';
 import Buttongroup from './button-group';
 
 
-Vue.component('g-button',Buttom)
+Vue.component('g-button',Button)
 Vue.component('g-button-group',Buttongroup)
 Vue.component('g-icon',Icon)
 
@@ -17,3 +17,91 @@ new Vue({
         loading3:false
     }
 })
+/*单元测试开始*/
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+
+const  expect = chai.expect
+/*测试按钮的icon*/
+{
+    const  Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-setting')
+    vm.$el.remove();
+    vm.$destroy()
+}
+/*测试按钮的loading状态*/
+{
+    const  Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+/*测试按钮的默认方向*/
+{
+    const div=document.createElement('div')
+    document.body.appendChild(div)
+    const  Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+/*测试按钮的修改方向*/
+{
+    const div=document.createElement('div')
+    document.body.appendChild(div)
+    const  Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            position:'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+/*测试按钮的click事件*/
+{
+    const  Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            position:'right'
+        }
+    })
+    vm.$mount()
+    let spy=chai.spy(function(){})
+    vm.$on('click',spy)
+    let button=vm.$el
+    button.click()
+    expect(spy).to.called()
+}
